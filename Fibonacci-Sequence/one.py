13a4ace70781b8d93b2a47204da982f32f17641f
varun2425 <varunmahi030@gmail.com>
2025-10-22 15:37:39 +0530

    py

---
+++ Fibonacci-Sequence/one.py
@@ -0,0 +1,8 @@
+
+
+def fib(n):
+    print(0)
+    print(1)
+
+
+fib(5)
---
+++ Fibonacci-Sequence/two.py
@@ -0,0 +1,15 @@
+
+
+def fib(n):
+
+    a =0 
+    b =1
+
+    print(a)
+    print(b)
+
+    for i in range(2,n):
+        print(a+b)
+
+
+fib(5)
