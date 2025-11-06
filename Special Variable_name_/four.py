27abd1542901530ce4ca0be1e5c668356d205067
varun2425 <varunmahi030@gmail.com>
2025-11-06 11:57:31 +0530

    py

---
+++ Special Variable_name_/four.py
@@ -0,0 +1,25 @@
+from Calc import add
+
+def fun1():
+    add()  
+    print("from fun1")
+
+
+def fun2():
+    print("from fun2")
+
+
+def main():
+    fun1()
+    fun2()
+
+main()
+
+
+
+# Out put is 
+# result 1 is
+# result 2 is
+# result 1 is
+# from fun1
+# from fun2
