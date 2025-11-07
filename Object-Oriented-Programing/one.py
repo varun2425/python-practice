22cd2bfe3c4b36ca42144fa637243023d11da46a
varun2425 <varunmahi030@gmail.com>
2025-11-07 12:11:32 +0530

    py

---
+++ Object-Oriented-Programing/one.py
@@ -0,0 +1,30 @@
+#oop  // python is famous thing beacuse og Object oriented Programing
+#python supports all different programming paradigm it supports
+# functional programming --> basically means if you can achieve a certain task implementing function as our mathematical functions ,where you will not manipulate a  DATA ,which is instead Changing data you will still accomplish a task ,One of the way is you can pass a function to function.......
+# object oriented programing
+# procedure Oriented Programming
+
+
+#Concept of OOPs
+
+#Object will have two things every object will have certain attributes and every object will have certain behavior
+#Attributes ---> attributr you can image them as data or you can say properties 
+#Example my height it's one attribute my age is one attribute.....
+#Object will have something that you can store data an object will have some behavior (iam talking , iam walking etc this are come under behaviour )
+
+#// if you want to store something an object we need to define variables
+#// if you want to define the behavior we need to use methods 
+# 
+# what is methods?
+#// function in object-oriented  programming they are called as methods
+
+
+#OOPs is not just about different syntax it's about the way you think //now if you want to achieve anything in programming you need to think about object 
+
+
+#OOP's have many concepts 
+#1)Object - Instance
+#2)Class - Design
+#3)Encapsulation
+#4)Abstraction
+#5)Polymorphism
