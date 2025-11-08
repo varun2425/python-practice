9e8f75aa59d359fc9ec47ecdb8f35d4c62249cb6
varun2425 <varunmahi030@gmail.com>
2025-11-08 11:38:50 +0530

    py

---
+++ Object-Oriented-Programing/Class-Object/one.py
@@ -0,0 +1,26 @@
+
+class Computer:
+
+    def config(self):
+        print("is , 16gb ,1TB")
+
+
+x =9 #Even this integer this X is an objedct of integer
+print(type(x))
+
+
+a = '8'
+print(type(a))
+
+com1 = Computer()       #COM1 here don't have a type we have to ,we have to  mention // Hey this COM1 is an object of computer  
+
+print(type(com1))
+
+
+
+#IN 2ND line we will write our stuff ,what stuff we can put in a class ,two thing 
+#1)ATTRIBUTES -->Variable ,the way you create variables
+#2)BEHAVIOUR  -->Methods[Function] 
+
+
+#String ia also class the same way computer class the difference is HDL is an inbuild class computer is an our class that how we define
