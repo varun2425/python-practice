5628049cc4245691002bfe4ebc2987b628c82e8a
varun2425 <varunmahi030@gmail.com>
2025-11-08 11:48:09 +0530

    py

---
+++ Object-Oriented-Programing/Class-Object/five.py
@@ -0,0 +1,19 @@
+class Computer():
+
+    def config(self):
+        print("i5 ,16gb, 1TB ")
+
+
+
+com1 = Computer()
+com2 = Computer()
+
+
+Computer.config(com1)
+Computer.config(com2)
+
+com1.config()
+com2.config()
+
+a = 5
+a.bit_length()
