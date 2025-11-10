5d56275419d7299fc37585195a9ee6e734992f43
varun2425 <varunmahi030@gmail.com>
2025-11-10 15:01:18 +0530

    py

---
+++ Object-Oriented-Programing/int_method/one.py
@@ -0,0 +1,28 @@
+
+class Computer():
+
+    def __init__(self):
+         print("in init")
+
+    def config(self):
+         print("i5 ,16gb, 1TB ")
+        
+
+
+
+
+com1 = Computer()
+com2 = Computer()
+
+
+Computer.config(com1)
+Computer.config(com2)
+
+
+
+
+
+
+
+
+
