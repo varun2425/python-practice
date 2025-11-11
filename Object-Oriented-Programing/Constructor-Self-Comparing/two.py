f932db85838e87357a4d679e814167ef3b720439
varun2425 <varunmahi030@gmail.com>
2025-11-11 11:53:20 +0530

    py

---
+++ Object-Oriented-Programing/Constructor-Self-Comparing/two.py
@@ -0,0 +1,20 @@
+#Here we are creating two objects c1 and c2
+
+class Computer:     #size of an object is Depends on te no.of variables 
+    pass
+
+
+c1 = Computer()
+c2 = Computer()
+
+print(id(c1)) #Every time you create an object it is allocated to new space
+print(id(c2))
+
+#Size of an object?//line number #3
+#Depends on the no.of variables and size of each variable
+
+
+#who allocates size to object ?
+#>Constructor line number 7,8
+
+
