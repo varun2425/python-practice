03ac63522736d74f20036397639fe3840907934f
varun2425 <varunmahi030@gmail.com>
2025-11-17 16:25:03 +0530

    py

---
+++ Inner-Class/one.py
@@ -0,0 +1,14 @@
+
+
+class Student:
+
+    def __init__(self,name,rollno):
+        self.name = name 
+        self.rollno = rollno
+
+
+
+s1 = Student('Varun',24)
+s2 = Student('Renuka',26)
+
+print(s1.name , s1.rollno)
