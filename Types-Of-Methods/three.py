1d53cf341b2f74a0299f47d4393833d328ae6f9c
varun2425 <varunmahi030@gmail.com>
2025-11-17 15:59:54 +0530

    py

---
+++ Types-Of-Methods/three.py
@@ -0,0 +1,33 @@
+
+
+class Student:
+
+    school = 'PVS'
+
+    def __init__(self,m1,m2,m3):
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+
+    def avg(self):
+        return(self.m1+self.m2+self.m3)/3
+    
+
+    @classmethod
+    
+    def getSchool(cls):
+        return cls.school
+
+    def info():
+        print("This is Student class .. in abc molude")
+
+
+s1 = Student(34,67,32)
+s2 = Student(89,32,12)
+
+
+
+print(s1.avg())
+print(Student.getSchool())
+
+Student.info()
