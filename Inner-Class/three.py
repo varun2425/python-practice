8de0701c1bc92eaf39771722cf58212909939f33
varun2425 <varunmahi030@gmail.com>
2025-11-19 10:20:14 +0530

    py

---
+++ Inner-Class/three.py
@@ -0,0 +1,30 @@
+
+class Student:
+
+    def __init__(self,name,rollno):
+        self.name = name 
+        self.rollno = rollno
+        self.lap = self.Laptop()
+
+    def show(self):
+        print(self.name,self.rollno)
+        self.lap.show()
+
+    class Laptop:
+
+        def __init__(self):
+            self.brand = 'DELL'
+            self.cpu = 'i5'
+            self.ram = 8
+
+        def show(self):
+            print(self.brand,self.cpu,self.ram)
+
+
+
+s1 = Student('Varun',24,)
+s2 = Student('Renuka',26)
+
+s1.show()
+
+lap1 = Student.Laptop()
